77d348bba0a4c3cae645625b671520b959fd6786
hassounamohamed <mohamed.hassouna.2004.24@gmail.com>
2025-01-02 22:48:55 +0100

    loginsignup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HelppComponent } from './helpp/helpp.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     { path: '', component:HomeComponent },
-    {path:'help', component:HelppComponent}
+    {path:'help', component:HelppComponent},
+    {path:'home' , component:HomeComponent},
+    {path:'login', component:LoginComponent},
+    {path:'signup',component:SignupComponent}
     
     
 ];
