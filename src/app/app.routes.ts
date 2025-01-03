abb67e15e0d1a3defce815233f66e9bb9749e59c
hassounamohamed <mohamed.hassouna.2004.24@gmail.com>
2025-01-03 22:32:01 +0100

    newbook and add component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { NewBookComponent } from './new-book/new-book.component';
 
 export const routes: Routes = [
     { path: '', component:HomeComponent },
     {path:'help', component:HelppComponent},
     {path:'home' , component:HomeComponent},
     {path:'login', component:LoginComponent},
-    {path:'signup',component:SignupComponent}
+    {path:'signup',component:SignupComponent},
+    {path:'add',component:NewBookComponent}
     
     
 ];
