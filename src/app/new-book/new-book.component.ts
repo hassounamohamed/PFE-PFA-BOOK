4f75e7587a508c75e41da5b6fd8942576411eefe
hassounamohamed <mohamed.hassouna.2004.24@gmail.com>
2025-01-04 22:14:44 +0100

    edit new book component

--- src/app/new-book/new-book.component.ts
+++ src/app/new-book/new-book.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-new-book',
-  imports: [FormsModule, MatFormFieldModule,MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule,MatInputModule, MatButtonModule,CommonModule],
   templateUrl: './new-book.component.html',
   styleUrl: './new-book.component.css'
 })
+
 export class NewBookComponent {
+  onCancel(form: NgForm): void{
+    if(confirm('Are you sure you want to cancel? All changes will be lost.')){
+      form.resetForm();
+    }
+  }
+  onSave(form: NgForm): void {
+    if (form.valid) {
+      const formData = form.value;
+      console.log('Book data:', formData);
+      alert('Book added successfully!');
+      form.resetForm();
+    } else {
+      alert('Please fill out all required fields!');
+    }
+  }
+
+
+
+
 
 }
